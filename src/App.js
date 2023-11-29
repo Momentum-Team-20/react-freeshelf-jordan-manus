673c885007fb3ce7b33be5b3e7d44dd07a26985c
Jordan Manus <jlmanus17@gmail.com>
2023-11-29 14:41:54 -0500

    added images folder in the public folder for the fallback image. added ability to change image if no image originally provided.

--- src/App.js
+++ src/App.js
@@ -33,12 +33,19 @@ function Display(props) {
   const handleClick = () => {
     setExpanded(!expanded)
   }
+
+  const fallback_image = "images/no-image-icon.png"
+  const addDefaultSrc = (ev) => {
+    ev.target.src = fallback_image
+  }
+
+
   return (
     <div>
       <h2 className=''>{props.title}</h2>
       <p className='author'>{props.author}</p>
       <p className='short-desc'>{props.shortDescription}</p>
-      <img className='image cover-art' src={props.image} />
+      <img className='image cover-art' src={props.image ? props.image : fallback_image} alt='book cover' onError={addDefaultSrc} />
       <button className='button' onClick={handleClick}>{expanded ? 'show less' : 'show more'}</button>
 
       {/* take div out if spacing is off */}
