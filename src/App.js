417f7ed053c4dae1123293d57805ae127b920d22
Jordan Manus <jlmanus17@gmail.com>
2023-11-29 13:42:33 -0500

    filled out the App function with fields to be viewed. Currently filling out the Display function. need to figure out props error being shown. add css.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import { useState } from 'react'
+import React from 'react'
 import './App.css'
 import bookData from './book-data.json'
+
 
 function App() {
   return (
     <div>
       <h1>Freeshelf</h1>
       {/* Use bookData to show books on the page*/}
+      <section className='section book-list'>
+        {bookData.map((book) => (
+          <Display
+            title={book.title}
+            author={book.author}
+            short_description={book.shortDescription}
+            image={book.coverImageUrl}
+            url={book.url}
+            publisher={book.publisher}
+            publication_date={book.publicationDate}
+            expanded_description={book.detailedDescription}
+          />
+        ))}
+      </section>
     </div>
   )
 }
+
+
+function Display(props) {
+  const [expanded, setExpanded] = useState(false)
+  const handleClick = () => {
+    setExpanded(!expanded)
+  }
+  return (
+    <h2 className=''>{props.title}</h2>
+  )
+}
+
 
 export default App
