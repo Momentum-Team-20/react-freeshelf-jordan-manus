dd972b2300e36da02ca3975e89a3a0e60dfd8d87
Jordan Manus <jlmanus17@gmail.com>
2023-11-29 16:01:12 -0500

    added bulma for css. added styling to images in app.css.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div>
       <h1>Freeshelf</h1>
       {/* Use bookData to show books on the page*/}
-      <section className='section book-list'>
+      <section className='section is-flex-wrap-wrap book-list'>
         {bookData.map((book) => (
           <Display
             title={book.title}
@@ -41,23 +41,31 @@ function Display(props) {
 
 
   return (
-    <div>
-      <h2 className=''>{props.title}</h2>
-      <p className='author'>{props.author}</p>
-      <p className='short-desc'>{props.shortDescription}</p>
-      <img className='image cover-art' src={props.image ? props.image : fallback_image} alt='book cover' onError={addDefaultSrc} />
-      <button className='button' onClick={handleClick}>{expanded ? 'show less' : 'show more'}</button>
+    <div className='columns'>
+      <div className='column is-two-thirds'>
+        <h2 className='title is-4 has-text-weight-bold'>{props.title}</h2>
+        <p className='author subtitle is-italic has-text-weight-medium'>{props.author}</p>
+        <p className='short-desc'>{props.short_description}</p>
 
-      {/* take div out if spacing is off */}
-      {expanded && (
-        <div>
-          <p className='link url'>{props.url}</p>
-          <p className='publisher'>{props.publisher}</p>
-          <p className='pub-date'>{props.publication_date}</p>
-          <p className='full-desc'>{props.expanded_description}</p>
-        </div>
-      )}
-    </div>
+
+
+        <button className='button is-small is-rounded' onClick={handleClick}>{expanded ? '^ show less' : '⌄ show more'}</button>
+
+        {/* take div out if spacing is off */}
+        {expanded && (
+          <div>
+            <p className='link url'><b>URL: </b>
+              <a href={props.url} className='is-link '>{props.url}</a>
+
+            </p>
+            <p className='publisher'><b>Publisher:</b> {props.publisher}</p>
+            <p className='pub-date'><b>Publication Date:</b> {props.publication_date}</p>
+            <p className='full-desc'><b>Full Description:</b><br /> {props.expanded_description}</p>
+          </div>)}
+      </div>
+
+      <img className='cover-art image column is-4by3 is-flex pt-1' src={props.image ? props.image : fallback_image} alt='book cover' onError={addDefaultSrc} />
+    </div >
   )
 }
 
