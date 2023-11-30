cca4e70d6bb7abeb9a42b8af4fb3e1da57cba580
Jordan Manus <jlmanus17@gmail.com>
2023-11-30 13:41:12 -0500

    removed nav from index.html and moved to app.js. removed main.js script tag from html. added animation to site name. added comments in app.js.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,72 @@ import { useState } from 'react'
 import React from 'react'
 import './App.css'
 import bookData from './book-data.json'
+import { sort_books, search_bar } from './main'
 
 
 function App() {
   return (
     <div>
-      <h1>Freeshelf</h1>
+      {/* nav bar */}
+      <div className='nav'>
+        <nav className="navbar has-background-black is-fixed-top" role="navigation" aria-label="main navigation">
+          <a className="navbar-item image" href="#"><img src="images/logo-for-book-project.png" className="logo-image is-square" /></a>
+          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+
+          {/* home and filter button */}
+          <div id="nav" className="navbar-menu is-spaced">
+            <div className="navbar-start">
+              <a className="navbar-item has-text-white home-link" href="#root">Home</a>
+              <div className="navbar-item">
+                <button className="button filter-button" onClick={sort_books}>Filter</button>
+              </div>
+            </div>
+
+            {/* search bar*/}
+            <div className="navbar-end has-addons mt-2">
+              <input id="searchbar" className="control input navbar-item" type="text" placeholder="search" />
+              <button className="control button is-info" onClick={search_bar}>Search</button>
+            </div>
+
+            {/* login buttons */}
+            <div className="navbar-end ml-0">
+              <div className="navbar-item">
+                <div className="buttons">
+                  <a className="button is-primary ">
+                    <strong>Sign up</strong>
+                  </a>
+                  <a className="button is-light">
+                    Log in
+                  </a>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      {/* banner animations */}
+      <div className='container'>
+        <div className='title-display'>
+          <h1 className='site-title'>Freeshelf</h1>
+        </div>
+        <div className='ghost'>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+
       {/* Use bookData to show books on the page*/}
-      <section className='section is-flex-wrap-wrap my-2 book-list'>
+      <ul className='section is-flex-wrap-wrap my-2 book-list'>
         {bookData.map((book) => (
           <Display
             title={book.title}
@@ -22,8 +80,8 @@ function App() {
             expanded_description={book.detailedDescription}
           />
         ))}
-      </section>
-    </div>
+      </ul>
+    </div >
   )
 }
 
@@ -39,41 +97,25 @@ function Display(props) {
     ev.target.src = fallback_image
   }
 
-
   return (
-    <div className='columns display'>
+
+
+    <li id='display' className='columns'>
       <div className='column is-two-thirds'>
-        <h2 className='title is-4 has-text-weight-bold'>{props.title}</h2>
-        <p className='author subtitle is-italic has-text-weight-medium'>{props.author}</p>
-        <p className='short-desc'>{props.short_description}</p>
+
+        {/* visible book info */}
+        <h2 id='book-title' className='title is-4 has-text-weight-bold'><b className='is-sr-only'>Title: </b>{props.title}</h2>
+        <p className='author subtitle is-italic has-text-weight-medium'><b className='is-sr-only'>Author: </b>{props.author}</p>
+        <p className='short-desc'><b className='is-sr-only'>Short Description: </b>{props.short_description}</p>
+
+        <button className='button is-small is-rounded' aria-expanded={expanded} onClick={handleClick}>{expanded ? '^ show less' : '⌄ show more'}</button>
 
 
-
-        <button className='button is-small is-rounded' onClick={handleClick}>{expanded ? '^ show less' : '⌄ show more'}</button>
-
-
-        {/* addind modal for expansion */}
-        {/* <div className="modal">
-          <div className="modal-background"></div>
-          <div className="modal-card">
-            <header className="modal-card-head">
-              <p className="modal-card-title">{props.author}</p>
-              <button className="delete" aria-label="close"></button>
-            </header>
-            <section className="modal-card-body">
-
-            </section>
-
-          </div>
-        </div> */}
-
-
-        {/* take div out if spacing is off */}
+        {/* hidden info until expanded button pressed */}
         {expanded && (
           <div>
             <p className='link url'><b>URL: </b>
               <a href={props.url} className='is-link '>{props.url}</a>
-
             </p>
             <p className='publisher'><b>Publisher:</b> {props.publisher}</p>
             <p className='pub-date'><b>Publication Date:</b> {props.publication_date}</p>
@@ -82,7 +124,8 @@ function Display(props) {
       </div>
 
       <img className='cover-art image column is-4by3 is-flex pt-1 my-3' src={props.image ? props.image : fallback_image} alt='book cover' onError={addDefaultSrc} />
-    </div >
+    </li >
+
   )
 }
 
--- src/main.js
+++ src/main.js
@@ -1,21 +1,44 @@
+import bookData from './book-data.json'
+
+
 console.log('connected');
 
-// allow user to sort by title, author, 
-// function sort_books() {
-//   // Declare variables
-//   var input, filter, ul, li, a, i;
-//   input = document.getElementById("searchbar");
-//   filter = input.value.toUpperCase();
-//   ul = document.getElementById("myMenu");
-//   li = ul.getElementsByTagName("li");
+let title = document.getElementById("book-title");
 
-//   // Loop through all list items, and hide those who don't match the search query
-//   for (i = 0; i < li.length; i++) {
-//     a = li[i].getElementsByTagName("a")[0];
-//     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-//       li[i].style.display = "";
-//     } else {
-//       li[i].style.display = "none";
-//     }
-//   }
+// allow user to sort by title, author, 
+export function search_bar() {
+    console.log('inside search bar');
+    // Declare variables
+    let input, filter, a, i, txtValue;
+    input = document.getElementById('searchbar');
+    filter = input.value.toUpperCase();
+    console.log(filter);
+    // title = document.getElementById("book-title");
+
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < title.length; i++) {
+        a = title[i].getElementsByTagName("")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            title[i].style.display = "";
+        } else {
+            title[i].style.display = "none";
+        }
+    }
+}
+
+
+export function sort_books() {
+    console.log('inside sort function');
+    bookData.sort(function (a, b) {
+        console.log(bookData[0].title);
+        return (a.title.toString - b.title.toString);
+    },
+
+    )
+}
+
+// function displayBooks() {
+//     document.getElementById('display')
 // }
